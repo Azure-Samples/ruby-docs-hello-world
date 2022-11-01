c63c951e99402e6f6fffc6e35eb669dc54b6e855
Kim Hee Su <heeboy007@naver.com>
2022-11-01 14:33:50 +0000

    env : complete 2.7.3 replit runnable env, test on azure

--- replit.nix
+++ replit.nix
@@ -1,17 +1,17 @@
 { pkgs }: 
 let
-  ruby_pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/860b56be91fb874d48e23a950815969a7b832fbc.tar.gz";
-  }) {};
-  glibc_pkgs = import (builtins.fetchTarball {
+  pkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/860b56be91fb874d48e23a950815969a7b832fbc.tar.gz";
   }) {};
 in {
   deps = [
     pkgs.nodejs-16_x
     pkgs.sqlite
-    ruby_pkgs.ruby
-    glibc_pkgs.glibc
+    pkgs.ruby
+    pkgs.libffi
+    pkgs.rubyPackages.ffi
+    pkgs.rubyPackages.ffi-compiler
+    pkgs.glibc
   ];
 }
 
