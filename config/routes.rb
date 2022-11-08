7222dee9320ae70c5927b4e85c3a99121eae7007
Kim Hee Su <heeboy007@naver.com>
2022-11-08 01:15:18 +0000

    test : add basic controller and test on azure server

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
