ee32439bd5bd7816b476eb1b0a80aff0da5e4c36
Kim Hee Su <heeboy007@naver.com>
2022-11-12 02:16:40 +0000

    env : try fix error on production mode, add rspec for this branch again

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ gem "rake", "~> 12.2"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "rspec-rails", "~> 3.4", ">= 3.4.2"
 end
 
 group :development do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-
+  config.serve_static_files = true
+  
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sassc
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
